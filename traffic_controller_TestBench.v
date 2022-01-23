2b446497c39d1039a01507a00301acc73690d523
Masi <81412933+masoume-pasebani@users.noreply.github.com>
2022-01-23 14:25:48 +0330

    Update traffic_controller_TestBench.v

--- traffic_controller_TestBench.v
+++ traffic_controller_TestBench.v
@@ -6,6 +6,30 @@ module Traffic_Controller_TB();
       wire [2:0] light_B;
 
       Traffic_Controller T_C(light_A,light_B,clk,reset);
+      initial clk = 1;
+      always #(1000000000/2) clk=~clk;
+       
+      initial begin
+      reset =1'b0;
+      #1000000000;
+
+      reset =1'b1;
+      #1000000000;
       
+      reset =1'b0;
+      #(1000000000*6);
+      
+      reset=1'b1;
+      #1000000000;
+    
+      reset=1'b0;
+      #(1000000000);
+
+      reset=1'b0;
+      #(1000000000);
+
+      $stop;
+      end
+
      
 endmodule
